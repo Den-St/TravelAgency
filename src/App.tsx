617023fb91bd610e5ef2ffd2fc6774b5d5419062
Nataliia Klapchuk <nataklap@gmail.com>
2025-05-09 14:50:07 +0300

    remove redundancies

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import { RoutesSwitch } from './app/routes';
 import { Layout } from './features/Layout/ui/Layout';
 
-import { Button } from './components/ui/Button/Button';
-
 function App() {
   return (
     <Layout>
       <RoutesSwitch />
-
-      <div className="space-y-4 p-4">
-        {/* Default Button */}
-        <Button>Default Button</Button>
-        <Button disabled>Default Button</Button>
-
-        {/* Secondary Button */}
-        <Button variant="secondary">Secondary Button</Button>
-
-        {/* Large Button */}
-        <Button size="full" disabled>
-          Large Disabled Button
-        </Button>
-        <Button size="full">Large Button</Button>
-
-        <Button variant="secondary" size="lg">
-          Sign in
-        </Button>
-      </div>
     </Layout>
   );
 }
