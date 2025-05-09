bb96fcac0833e232d5b4838b3d79be3f81eb4734
Nataliia Klapchuk <nataklap@gmail.com>
2025-05-09 21:39:56 +0300

    fix shadow color

--- src/components/ui/ShadowWrapper/ShadowWrapper.tsx
+++ src/components/ui/ShadowWrapper/ShadowWrapper.tsx
@@ -13,7 +13,7 @@ export const ShadowWrapper: FC<ShadowWrapperProps> = ({
   return (
     <div
       data-testid="shadow-wrapper"
-      className={`p-6 w-full bg-white shadow-[0px_2px_10px_6px_rgba(2,126,172,0.2)] rounded-[12px] ${className}`}
+      className={`p-6 w-full bg-white shadow-[0px_2px_10px_6px_rgba(2,126,172,0.1)] rounded-[12px] ${className}`}
       {...props}
     >
       {children}
