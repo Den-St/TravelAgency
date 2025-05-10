9812d7d166c7a332f0b58cf0106875932ee3cecb
Nataliia Klapchuk <nataklap@gmail.com>
2025-05-10 15:12:57 +0300

    add Info icon

--- src/shared/ui-kit/Icons.tsx
+++ src/shared/ui-kit/Icons.tsx
@@ -1,8 +1,11 @@
-export { LuEye as Eye, LuWallet as Wallet } from 'react-icons/lu';
+export {
+  LuEye as Eye,
+  LuWallet as Wallet,
+  LuInfo as Info,
+} from 'react-icons/lu';
 export { IoMdEyeOff as EyeCrossed } from 'react-icons/io';
 export { FaStar as Star } from 'react-icons/fa6';
 export { IoCheckmarkOutline as CheckMark } from 'react-icons/io5';
 export { FiCalendar as Calendar } from 'react-icons/fi';
-export { CiCircleInfo as Info } from 'react-icons/ci';
 export { MealIcon as Meal } from './Icons/MealIcon';
 export { PlaceIcon as Place } from './Icons/PlaceIcon';
